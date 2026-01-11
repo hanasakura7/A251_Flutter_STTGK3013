2c734595e30cb123afa5bbab835787a604098da4
Hana Syakirah <hanasyakirah@gmail.com>
2026-01-11 14:36:00 +0800

    Update user.dart

--- pawpal (final, tbc)/lib/models/user.dart
+++ pawpal (final, tbc)/lib/models/user.dart
@@ -4,11 +4,7 @@ class User {
   String? userName;
   String? userPhone;
   String? userPassword;
-  String? userOtp;
   String? userRegdate;
-  String? userAddress; // NEW
-  String? userLatitude; // Existing new
-  String? userLongitude; // Existing new
   int? userCredit;
 
   String? userProfileImage;
@@ -19,11 +15,7 @@ class User {
     this.userName,
     this.userPhone,
     this.userPassword,
-    this.userOtp,
     this.userRegdate,
-    this.userAddress,
-    this.userLatitude,
-    this.userLongitude,
     this.userCredit,
   });
 
@@ -33,15 +25,8 @@ class User {
     userName = json['user_name'];
     userPhone = json['user_phone'];
     userPassword = json['user_password'];
-    userOtp = json['user_otp'];
     userRegdate = json['user_regdate'];
-
-    userAddress = json['user_address']; // NEW
-    userLatitude = json['user_latitude']; // NEW
-    userLongitude = json['user_longitude']; // NEW
-
-    userCredit = int.parse(json['user_credit']);
-
+    userCredit = int.tryParse(json['user_credit']?.toString() ?? '0') ?? 0;
     userProfileImage = json['profile_image'];
   }
 
@@ -52,15 +37,9 @@ class User {
     data['user_name'] = userName;
     data['user_phone'] = userPhone;
     data['user_password'] = userPassword;
-    data['user_otp'] = userOtp;
     data['user_regdate'] = userRegdate;
 
-    data['user_address'] = userAddress; // NEW
-    data['user_latitude'] = userLatitude; // NEW
-    data['user_longitude'] = userLongitude; // NEW
-
     data['user_credit'] = userCredit;
-
     data['profile_image'] = userProfileImage;
     return data;
   }
